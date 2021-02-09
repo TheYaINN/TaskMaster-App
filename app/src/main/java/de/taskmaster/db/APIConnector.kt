2f9c28c48f6fdb68333a1306fc5a958da2d81fbd
unknown <christiankoller26.9@gmail.com>
2021-02-09 10:17:49 +0100

    reworked http requests

--- app/src/main/java/de/taskmaster/db/APIConnector.kt
+++ app/src/main/java/de/taskmaster/db/APIConnector.kt
@@ -2,6 +2,8 @@ package de.taskmaster.db
 
 interface APIConnector {
 
-    fun postRequest(vararg arguments: String): String
+    fun postRequest(specifiedPath: String, body: String, parameters: Map<String, String>): String
+
+    fun getRequest(specifiedPath: String, parameters: Map<String, String>): String
 
 }
--- app/src/main/java/de/taskmaster/db/ServerConnector.kt
+++ app/src/main/java/de/taskmaster/db/ServerConnector.kt
@@ -1,36 +1,57 @@
 package de.taskmaster.db
 
-import java.io.BufferedInputStream
-import java.io.BufferedOutputStream
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
 import java.net.URL
+
+fun main() {
+    val response = ServerConnector.INSTANCE.getRequest(
+        "/login/register/checkForExisting",
+        mapOf(Pair("username", "test"), Pair("mail", "test"))
+    )
+
+    print(response)
+}
 
 class ServerConnector : APIConnector {
 
-    private val server = "217.160.242.136:8085"
+    private val server = "http://localhost:8085"
 
-    override fun postRequest(vararg arguments: String): String {
-        val url = URL("$server/${arguments.first()}")
-        val connection = url.openConnection()
-        val out = BufferedOutputStream(connection.getOutputStream())
+    override fun postRequest(
+        specifiedPath: String,
+        body: String,
+        parameters: Map<String, String>
+    ): String {
+        val client = OkHttpClient()
 
-        val writer = BufferedWriter(OutputStreamWriter(out, "UTF-8"))
-        arguments.drop(1).forEach { writer.write(it) }
-        writer.flush()
-        writer.close()
-        out.close()
-        val input = BufferedInputStream(connection.getInputStream())
+        val url = buildUrl(specifiedPath, parameters)
+        val requestBody = body.toRequestBody("application/json".toMediaType())
+        val request = Request.Builder().url(url).post(requestBody).build()
 
-        val response = BufferedReader(InputStreamReader(input, "UTF-8"))
-        response.readLine()
+        val response = client.newCall(request).execute()
+        return BufferedReader(response.body?.charStream()).readLine()
+    }
 
-        connection.connect()
-        response.close()
+    //specifiedPath starts with /
+    //parameter.key is the parametername and parameters.value is the parameter value
+    override fun getRequest(specifiedPath: String, parameters: Map<String, String>): String {
+        val client = OkHttpClient()
 
-        return response.readText()
+        val url = buildUrl(specifiedPath, parameters)
+        val request = Request.Builder().url(url).build()
+
+        val response = client.newCall(request).execute()
+        return BufferedReader(response.body?.charStream()).readLine()
+    }
+
+    private fun buildUrl(specifiedPath: String, parameters: Map<String, String>): String {
+        val builder = URL(server + specifiedPath).toHttpUrlOrNull()?.newBuilder() ?: error("error")
+        parameters.forEach { entry -> builder.addQueryParameter(entry.key, entry.value) }
+        return builder.build().toString()
     }
 
     companion object {
